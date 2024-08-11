f62214e59a11da14adea3050cf74dfb81d45efb1
zack781 <thean781@gmail.com>
2024-08-11 19:30:25 -0400

    Implemented brush

--- paint.py
+++ paint.py
@@ -16,8 +16,17 @@ def main():
     # screen.fill(WHITE)
     screen.fill(GREY)
 
+    mouse_prev_x = 0
+    mouse_prev_y = 0
+    mouse_down = False
     # Inifinite loop
     while (running):
+        if (mouse_down):
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            if (mouse_prev_x != 0 and mouse_prev_y != 0):
+                pg.draw.line(screen, (0, 0, 0), (mouse_prev_x, mouse_prev_y), (mouse_x, mouse_y))
+            mouse_prev_x = mouse_x
+            mouse_prev_y = mouse_y
         # Event listener
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -25,6 +34,12 @@ def main():
                 raise SystemExit
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 print("Mouse button clicked!")
+                mouse_down = True
+            if event.type == pg.MOUSEBUTTONUP:
+                print("Mouse button up!")
+                mouse_down = False
+                mouse_prev_x = 0
+                mouse_prev_y = 0
             if event.type == pg.WINDOWRESIZED:
                 screen = pg.display.get_surface()
                 screen.fill(WHITE)
